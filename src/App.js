b19e234354acb62b319295902f789b284bd4dde2
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-17 13:58:20 +0530

    bar chart added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,31 @@ export const data = [
 export const options = {
   title: "Projects"
 };
+export const data1 = [
+  [
+    "label",
+    "valu",
+    { role: "style" },
+    {
+      sourceColumn: 0,
+      role: "annotation",
+      type: "string",
+      calc: "stringify"
+    }
+  ],
+
+  ["Active Branches", 10, "silver", null],
+  ["Commits", 21, "color: #e5e4e2", null]
+];
+
+export const options1 = {
+  title: "Projects- Branching Strategy",
+  width: 400,
+  height: 200,
+  bar: { groupWidth: "65%" },
+  legend: { position: "none" }
+};
+
 export default function App() {
   return (
     <div className="App">
@@ -19,6 +44,13 @@ export default function App() {
         options={options}
         width={"100%"}
         height={"400px"}
+      />
+      <Chart
+        chartType="BarChart"
+        width="100%"
+        height="100px"
+        data={data1}
+        options={options1}
       />
     </div>
   );
