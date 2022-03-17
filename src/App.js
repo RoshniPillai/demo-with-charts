87e6013134ce687162009eff83ce692bc0993d12
RoshniPillai <99415888+RoshniPillai@users.noreply.github.com>
2022-03-17 13:40:36 +0530

    charts added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import { Chart } from "react-google-charts";
 
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Success", 70],
+  ["Failure", 30]
+];
+
+export const options = {
+  title: "Projects"
+};
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </div>
   );
 }
